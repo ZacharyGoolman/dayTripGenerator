da1f48bb9689231eddfee6504673922569ed54e7
ZacharyGoolman <zachary.goolman@gmail.com>
2022-03-15 15:19:13 -0400

    Created function is list (Destinations) so that user can select location to visit

--- main.py
+++ main.py
@@ -3,27 +3,55 @@
 
 Destinations = ["New York" , "Washington" , "North Carolina" , "Ohio"]
 
-Restaurants = ["Aces perfect Pizza" , "Barmini" , "Mayflower Seafood" , "Pies's And Pints"]
-
 Mode_of_transportation = ["Rental car" , "Subway" , "Bus" , "Limousine"]
+
+Restaurants = ["Aces perfect Pizza" , "Barmini" , "Mayflower Seafood" , "Pies's And Pints"]
 
 Entertainment = ["Broadway musical" , "Indy car race" , "Hike Mt Rainier" , "Snowboarding"]
 
+#Destinations  list Function
+import random
 
-# import random
-# print(random.choice(Destinations))
+location = (random.choice(Destinations))
+print(location)
 
-def trip_location(response):
-   result = True or False
-   return result
+def destination():
+   
+   users_choice = ("No")
+   
+   while users_choice == ("No"):
+      
+      users_choice = (input("Does this destination sound Ideal?"))
+      
+      if users_choice == ("Yes"):
+         print("Awesome I'm glad that locatin worked out!")
+      
+      elif users_choice == ("No"):
+         location = (random.choice(Destinations))
+         print("How do this location sound" + " " + location)
+      
+destination()         
 
-trip_location("Yes", "No")
-
-       
   
-    
-    # print(random.choice(Restaurants))
+#    if users_choice == True: 
+#       print("Awesome, I'm glad we found your destination!")
+#    else:
+#       print("I'm sorry lets try another selection")
 
-    # print(random.choice(Mode_of_transportation))
 
-    # print(random.choice(Entertainment))
+# #call my function
+
+# print("Would" " " + random.choice(Destinations) +  " " "be better suited for your travel?") 
+# print("Ok great lets find you some Transportation!")
+
+
+
+
+
+
+
+
+
+# print(random.choice(Mode_of_transportation))
+# print(random.choice(Restaurants))
+# print(random.choice(Entertainment))
