2e2486bc8d4f52d4d9553ac681b8951ca15facd3
ZacharyGoolman <zachary.goolman@gmail.com>
2022-03-15 18:05:57 -0400

    Created Confirmation function and display fucntion for user

--- main.py
+++ main.py
@@ -61,8 +61,10 @@ food = (random.choice(Restaurants))
 print(food)
 
 def choice_of_food():
+   
    users_choice = ("No")
    while users_choice == ("No"):
+      
       users_choice =(input("Does this dining option sound like a good choice?"))
       
       if users_choice == ("Yes"):
@@ -80,8 +82,10 @@ enjoyment = (random.choice(Entertainment))
 print(enjoyment)
 
 def activities():
+   
    users_choice = ("No")
    while users_choice == ("No"):
+      
       users_choice =(input("Does this activity sound enjoyable?"))
       
       if users_choice == ("Yes"):
@@ -92,3 +96,25 @@ def activities():
          print("Does" + " " + enjoyment + " " + "sound to you?")
 
 activities()
+
+
+def confirm():
+   print("We are excited to tell you that you have completed all the necessary step required of you and now you can relax knowing that your vacation is planned")
+   users_choice = ("No")
+   while users_choice == ("No"):
+      
+      users_choice = (input("Please Re read your selections above and confirm with a simple Yes or No that what is listed is correct and too your satisfaction"))
+      if users_choice == ("Yes"):
+         print("Thank you for your comnfirmation")
+      elif users_choice == ("No"):
+         print("Thats ok let's start over and create your dream day no need to settle!")
+
+
+confirm()
+
+def display():
+   print("As confirmed above we are excited to present to you a breakdown of your completed Vacation day trip and we know you are going to have a wonderful time. Thank you for using DayTrip and we hope you have the best randomly created vacation ever!")
+   for items in location ,transportation ,food, enjoyment:
+      print(items)
+
+display()
