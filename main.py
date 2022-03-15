2004eff07803c5bc10bba7c888a6a44509c98d41
ZacharyGoolman <zachary.goolman@gmail.com>
2022-03-15 12:23:19 -0400

    Created main.py File for project

---
+++ main.py
@@ -0,0 +1,29 @@
+# make 4 lists. lists should be the only thing outside of the function. 
+
+
+Destinations = ["New York" , "Washington" , "North Carolina" , "Ohio"]
+
+Restaurants = ["Aces perfect Pizza" , "Barmini" , "Mayflower Seafood" , "Pies's And Pints"]
+
+Mode_of_transportation = ["Rental car" , "Subway" , "Bus" , "Limousine"]
+
+Entertainment = ["Broadway musical" , "Indy car race" , "Hike Mt Rainier" , "Snowboarding"]
+
+
+# import random
+# print(random.choice(Destinations))
+
+def trip_location(response):
+   result = True or False
+   return result
+
+trip_location("Yes", "No")
+
+       
+  
+    
+    # print(random.choice(Restaurants))
+
+    # print(random.choice(Mode_of_transportation))
+
+    # print(random.choice(Entertainment))
